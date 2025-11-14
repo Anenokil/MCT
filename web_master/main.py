f7baabe2600fa6203144237941bb9f8c58981062
Anenokil <anenokil.w@gmail.com>
2025-11-14 14:34:49 +0300

    Feat: store client IP addresses

--- web_master/main.py
+++ web_master/main.py
@@ -1,14 +1,20 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 ping_count = 0
+ip_addresses = set()
 
 
 @app.route('/ping')
 def ping():
-    global ping_count
+    global ping_count, ip_addresses
+
     ping_count += 1
+
+    client_ip = request.remote_addr
+    ip_addresses.add(client_ip)
+
     return 'pong'
 
 
