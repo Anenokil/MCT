1e0846dd34982611663865754551f55f25e188f5
Anenokil <anenokil.w@gmail.com>
2025-11-14 14:03:55 +0300

    Feat: add `/visits` endpoint returning number of ping call count

--- web_master/main.py
+++ web_master/main.py
@@ -2,10 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+ping_count = 0
+
 
 @app.route('/ping')
 def ping():
+    global ping_count
+    ping_count += 1
     return 'pong'
+
+
+@app.route('/visits')
+def visits():
+    return f'{ping_count}'
 
 
 if __name__ == '__main__':
